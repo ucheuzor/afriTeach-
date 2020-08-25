292bd96448ed40c9643c73e6f011158d922a14b9
uzor uchenna <ucheuzor24@gmail.com>
2020-08-25 17:34:21 +0100

    AfriTeach boilerPlate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='containter'>
+      <div className="jumbotron text-center">
+        <h1>AfriTeach website</h1>
+        <p className="lead">Welcome to Afriteach, A platform that helps make learning easy in Africa.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Get Started</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
